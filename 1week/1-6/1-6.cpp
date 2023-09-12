b260231b5ef49da827e452815e841664b065593b
minachoi <choime321@gmail.com>
2023-09-13 03:59:39 +0900

    넘어가면 반대로 나오게 수정하기

--- 1week/1-6/1-6.cpp
+++ 1week/1-6/1-6.cpp
@@ -4,7 +4,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-#define BOARDSIZE 30
+#define BOARDSIZE 10
+int board[BOARDSIZE][BOARDSIZE];
 
 class Point
 {
@@ -15,6 +16,8 @@ public:
 	void InputPoint();
 	int GetX();
 	int GetY();
+	bool SetX(const int& x);
+	bool SetY(const int& y);
 };
 
 Point::Point() : x{ 0 }, y{ 0 } { }
@@ -33,15 +36,73 @@ int Point::GetY()
 	return y;
 }
 
+bool Point::SetX(const int& x)
+{
+	if (x < 0) {
+		return false;
+	}
+
+	this->x = x;
+	return true;
+}
+
+bool Point::SetY(const int& y)
+{
+	if (y < 0) {
+		return false;
+	}
+
+	this->y = y;
+	return true;
+}
+
 class Rec
 {
 private:
 	Point p1, p2;
 public:
+	void ResetBoard();
 	void InputPoint();
 	void PrintBoard();
-	void Move();
+	void UpDownMove(const char& m);
+	void LRMove(const char& m);
+	void Zoom(const char& m);
+	void XZoom(const char& m);
+	void YZoom(const char& m);
 };
+
+void Rec::ResetBoard()
+{
+	// 보드 초기화
+	for (int i = 0; i < BOARDSIZE; ++i) {
+		for (int j = 0; j < BOARDSIZE; ++j) {
+			board[i][j] = 0;
+		}
+	}
+
+	////// board 넘어갔을 때
+	//if (p1.GetX() > p2.GetX()) {
+	//	for (int i = 0; i < p2.GetX(); ++i) {
+	//		for (int j = p1.GetX(); j < p2.GetY(); ++j) {
+	//			board[i][j] = 1;
+	//		}
+	//	}
+	//}
+	//if (p1.GetY() > p2.GetY()) {
+	//	for (int i = p1.GetX(); i < p1.GetX(); ++i) {
+	//		for (int j = 0; j < p2.GetY(); ++j) {
+	//			board[i][j] = 1;
+	//		}
+	//	}
+	//}
+
+	// 보드에 표시
+	for (int i = p1.GetX(); i < p2.GetX(); ++i) {
+		for (int j = p1.GetY(); j < p2.GetY(); ++j) {
+			board[i][j] = 1;
+		}
+	}
+}
 
 void Rec::InputPoint()
 {
@@ -49,8 +110,6 @@ void Rec::InputPoint()
 	p1.InputPoint();
 	p2.InputPoint();
 }
-
-
 
 void Menu()
 {
@@ -64,11 +123,12 @@ void Menu()
 
 void Rec::PrintBoard()
 {
+	ResetBoard();
+
 	for (int i = 0; i < BOARDSIZE; ++i) {
 		for (int j = 0; j < BOARDSIZE; ++j) {
 			cout << "    ";
-			// p1 이상 p2 이하일때
-			if ((i >= p1.GetX() && j >= p1.GetX()) && (i <= p2.GetX() && j <= p2.GetX())) {
+			if (board[i][j] != 0) {
 				cout << "0";
 			}
 			else {
@@ -79,9 +139,104 @@ void Rec::PrintBoard()
 	}
 }
 
-void Rec::Move()
+void Rec::UpDownMove(const char& m)
 {
-	
+	if (m == 'w') {
+		// 만약 음수면
+		if ((p1.GetX() - 1 < 0) || (p2.GetX() - 1 < 0)) {
+			if (p1.GetX() - 1 < 0)
+				p1.SetX(BOARDSIZE - 1);
+			else if(p2.GetX() - 1 < 0)
+				p2.SetX(BOARDSIZE - 1);
+		}
+		else {
+			p1.SetX(p1.GetX() - 1);
+			p2.SetX(p2.GetX() - 1);
+		}
+	}
+	else if (m == 's') {
+		// 만약 보드를 넘으면
+		if ((p1.GetX() + 1 > BOARDSIZE) || (p2.GetX() + 1 > BOARDSIZE)) {
+			p1.SetX(0);
+			p2.SetX(0);
+		}
+		else {
+			p1.SetX(p1.GetX() + 1);
+			p2.SetX(p2.GetX() + 1);
+		}
+	}
+}
+
+void Rec::LRMove(const char& m)
+{
+	if (m == 'a') {
+		// 만약 음수면
+		if ((p1.GetY() - 1 < 0) || (p2.GetY() - 1 < 0)) {
+			if (p1.GetY() - 1 < 0)
+				p1.SetY(BOARDSIZE - 1);
+			else if (p2.GetY() - 1 < 0)
+				p2.SetY(BOARDSIZE - 1);
+		}
+		else {
+			p1.SetY(p1.GetY() - 1);
+			p2.SetY(p2.GetY() - 1);
+		}
+	}
+	else if (m == 'd') {
+		// 만약 보드를 넘으면
+		if ((p1.GetY() + 1 > BOARDSIZE) || (p2.GetY() + 1 > BOARDSIZE)) {
+			p1.SetY(0);
+			p2.SetY(0);
+		}
+		else {
+			p1.SetY(p1.GetY() + 1);
+			p2.SetY(p2.GetY() + 1);
+		}
+	}
+}
+
+void Rec::Zoom(const char& m)
+{
+	if (m == 'e') {
+		if ((p1.GetX() != p2.GetX() - 1) && (p1.GetY() != p2.GetY() - 1)) {
+			p2.SetX(p2.GetX() - 1);
+			p2.SetY(p2.GetY() - 1);
+		}
+	}
+	else if (m == 'E') {
+		if ((BOARDSIZE > p2.GetX()) && (BOARDSIZE > p2.GetY())) {
+			p2.SetX(p2.GetX() + 1);
+			p2.SetY(p2.GetY() + 1);
+		}
+	}
+}
+
+void Rec::XZoom(const char& m)
+{
+	if (m == 'j') {
+		if ((p1.GetY() != p2.GetY() - 1)) {
+			p2.SetY(p2.GetY() - 1);
+		}
+	}
+	else if (m == 'i') {
+		if ( (BOARDSIZE > p2.GetY())) {
+			p2.SetY(p2.GetY() + 1);
+		}
+	}
+}
+
+void Rec::YZoom(const char& m)
+{
+	if (m == 'l') {
+		if ((p1.GetX() != p2.GetX() - 1)) {
+			p2.SetX(p2.GetX() - 1);
+		}
+	}
+	else if (m == 'k') {
+		if ((BOARDSIZE > p2.GetX())) {
+			p2.SetX(p2.GetX() + 1);
+		}
+	}
 }
 
 int main()
@@ -95,24 +250,34 @@ int main()
 		char m;	cin >> m;
 		switch (m) {
 		case 'w':
+			r.UpDownMove('w');
 			break;
 		case 's':
+			r.UpDownMove('s');
 			break;
 		case 'a':
+			r.LRMove('a');
 			break;
 		case 'd':
+			r.LRMove('d');
 			break;
 		case 'e':
+			r.Zoom('e');
 			break;
 		case 'E':
+			r.Zoom('E');
 			break;
 		case 'i':
+			r.XZoom('i');
 			break;
 		case 'j':
+			r.XZoom('j');
 			break;
 		case 'k':
+			r.YZoom('k');
 			break;
 		case 'l':
+			r.YZoom('l');
 			break;
 		case 'q':
 			cout << "프로그램을 종료합니다." << endl;
