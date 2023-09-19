da4326f3e49fde8061d11e5adfa99e68c52f48f8
minachoi <choime321@gmail.com>
2023-09-19 23:23:37 +0900

    task1 2-1로 이름 변경, 2-2 생성 및 초기 코드

---
+++ 2week/2-2/2-2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <gl/glew.h> //--- 필요한 헤더파일 include
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+int winID;
+
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{ //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 디스플레이 모드 설정
+	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
+	glutInitWindowSize(800, 600); // 윈도우의 크기 지정
+	winID = glutCreateWindow("실습 1"); // 윈도우 생성(윈도우 이름)
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) // glew 초기화
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutMainLoop(); // 이벤트 처리 시작
+}
+
+/*
+윈도우를 띄우고 마우스 명령을 실행 해 본다.
+ 화면의 가로 세로를 각각 2등분하여 사각형 4개를 그린다.
+ 네 개의 사각형에 마우스를 클릭하여 색상과 크기를 바꾼다.
+ 왼쪽 마우스 클릭
+ 사각형 내부 클릭: 사각형 색상을 랜덤하게 바꾸기
+ 사각형 외부 클릭: 배경색을 랜덤하게 바꾸기
+ 오른쪽 마우스 클릭
+ 사각형 내부 클릭: 사각형 크기 축소
+ 사각형 외부 클릭: 사각형 크기 확대
+ 사각형 그리기 함수
+ void glRectf (Glfloat x1, Glfloat y1, Glfloat x2, Glfloat y2);
+ (x1, y1): 좌측 하단 좌표값
+ (x2, y2): 우측 상단 좌표값
+ 사각형 색상 바꾸기
+ void glColor3f (Glfloat r, Glfloat g, Glfloat b);
+ 현재 색상 설정하기
+ (r, g, b): red, green, blue 색상, 0.0 ~ 1.0 사이의 값으로 glRectf 를 호출하기 직전 설정한다.
+** Modern openGL 에서는 사용할 수 없는 deprecated 함수
+** 셰이더를 사용하게 되면 이 함수는 사용할 수 없음
+*/
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // 바탕색을 ‘blue’ 로 지정
+	glClear(GL_COLOR_BUFFER_BIT); // 설정된 색으로 전체를 칠하기
+	// 그리기 부분 구현: 그리기 관련 부분이 여기에 포함된다.
+	glutSwapBuffers(); // 화면에 출력하기
+}
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
